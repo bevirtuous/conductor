01df008beeda228a80f86f25f52c2a44e52452ff
devbucket <florian.mueller81@gmx.de>
2018-01-12 15:29:28 +0100

    Changed reports handling.

--- sendReports.js
+++ sendReports.js
@@ -1,20 +1,37 @@
 const coveralls = require('@sourceallies/coveralls-merge');
+const { existsSync } = require('fs');
+
+const reports = [];
 
 /**
  * Adds a new report file.
  * @param {string} packageName The package name.
- * @return {Oject}
  */
-const addReport = packageName => ({
-  type: 'lcov',
-  reportFile: `./packages/${packageName}/build/lcov.info`,
-});
+const addReport = (packageName) => {
+  const reportFile = `./packages/${packageName}/build/lcov.info`;
 
-coveralls.sendReports([
-  addReport('conductor'),
-  addReport('conductor-events'),
-  addReport('conductor-helpers'),
-  addReport('react-conductor'),
-  addReport('react-conductor-transitions'),
-  addReport('redux-conductor'),
-]);
+  if (!existsSync(reportFile)) {
+    return;
+  }
+
+  reports.push({
+    type: 'lcov',
+    reportFile: `./packages/${packageName}/build/lcov.info`,
+  });
+};
+
+addReport('conductor');
+addReport('conductor-events');
+addReport('conductor-helpers');
+addReport('react-conductor');
+addReport('react-conductor-transitions');
+addReport('redux-conductor');
+
+try {
+  if (reports.length) {
+    coveralls.sendReports(reports);
+    console.log('Reports send!');
+  }
+} catch (err) {
+  console.error(err);
+}
