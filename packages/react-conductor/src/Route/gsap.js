bf36057f5d1afbc36cc8d46f8cbac77d3e39e259
Richard Gorman <richardgorman@outlook.com>
2018-05-23 11:05:17 +0200

    Disabled transitions

--- packages/react-conductor/src/Route/gsap.js
+++ packages/react-conductor/src/Route/gsap.js
@@ -1,92 +1,27 @@
-const forward = {
-  duration: 375,
-  default: {
-    height: '100vh',
-    left: '0',
-    position: 'absolute',
-    top: '0',
-    transform: 'translate3d(100%, 0, 0)',
-    width: '100%',
-    willChange: 'transform',
-  },
-  entering: {
-    pointerEvents: 'none',
-    transform: 'none',
-  },
-  entered: {
-    transform: 'none',
-  },
-  exiting: {
-    pointerEvents: 'none',
-    transform: 'translate3d(100%, 0, 0)',
-  },
-  exited: {
-    pointerEvents: 'none',
-    transform: 'translate3d(100%, 0, 0)',
-  },
-};
-
-const backward = {
-  duration: 375,
-  default: {
-    height: '100vh',
-    left: '0',
-    position: 'absolute',
-    top: '0',
-    transform: 'translate3d(-50%, 0, 0)',
-    width: '100%',
-    willChange: 'transform',
-  },
-  entering: {
-    pointerEvents: 'none',
-    transform: 'none',
-  },
-  entered: {
-    transform: 'none',
-  },
-  exiting: {
-    pointerEvents: 'none',
-    transform: 'translate3d(-50%, 0, 0)',
-  },
-  exited: {
-    pointerEvents: 'none',
-    transform: 'translate3d(-50%, 0, 0)',
-  },
-};
-
-const replace = {
-  duration: 375,
-  default: {
-    height: '100vh',
-    left: '0',
-    position: 'absolute',
-    top: '0',
-    transform: 'none',
-    transition: 'opacity 375ms cubic-bezier(0.25, 0.1, 0.25, 1), transform 375ms cubic-bezier(0.25, 0.1, 0.25, 1)',
-    width: '100%',
-    willChange: 'transform',
-  },
-  entering: {
-    pointerEvents: 'none',
-    transform: 'none',
-  },
-  entered: {
-    transform: 'none',
-  },
-  exiting: {
-    opacity: '0.2',
-    pointerEvents: 'none',
-    transform: 'translate3d(0, -10%, 0)',
-  },
-  exited: {
-    opacity: 0,
-    pointerEvents: 'none',
-    transform: 'translate3d(0, -10%, 0)',
-  },
-};
-
 export default {
-  forward,
-  backward,
-  replace,
+  duration: 375,
+  pop: {
+    in: {
+      x: '0%',
+    },
+    out: {
+      x: '0%',
+    },
+  },
+  push: {
+    in: {
+      x: '0%',
+    },
+    out: {
+      x: '0%',
+    },
+  },
+  replace: {
+    in: {
+      x: '0%',
+    },
+    out: {
+      x: '0%',
+    },
+  },
 };
--- packages/react-conductor/src/Route/index.jsx
+++ packages/react-conductor/src/Route/index.jsx
@@ -62,9 +62,32 @@ class Route extends Component {
    * 
    */
   componentDidMount() {
-    // console.warn(this.node);
-    // TweenLite.to(this.node.current, 5, { css: { backgroundColor: "#F19939", height: '100vh' } });
+    // const start = transition.push.out;
+    // TweenLite.to(this.node.current, 0, start);
   }
+
+  /**
+   * 
+   * @param {*} nextProps 
+   */
+  // componentWillReceiveProps(nextProps) {
+  //   //
+  //   if (this.props.isVisible === nextProps.isVisible) {
+  //     return;
+  //   }
+
+  //   // Then find out which history action was just fired (use helper)
+  //   const action = getCurrentAction();
+
+  //   const position = this.props.isVisible ? 'in' : 'out';
+  //   const newPosition = nextProps.isVisible ? 'in' : 'out';
+  //   console.warn(position, newPosition, action);
+
+  //   const start = transition[action.toLowerCase()][position];
+  //   const end = transition[action.toLowerCase()][newPosition];
+
+  //   TweenLite.fromTo(this.node.current, 300 / 1000, start, end);
+  // }
 
   /**
    * 
@@ -81,11 +104,49 @@ class Route extends Component {
   get transitionType() {
     if (this.props.path && !this.props.isVisible) {
       return transition.backward;
-    } else if (getCurrentAction() === 'replace') {
+    } else if (getCurrentAction() === 'REPLACE') {
       return transition.replace;
     }
 
     return transition.forward;
+  }
+
+  setPosition = (props, duration = transition.duration) => {
+    const start = this.getStartPosition(props);
+    // let position;
+
+    // if (props.path) {
+    //   if (props.isVisible) {
+    //     position = transition.visible;
+    //   } else {
+    //     position = transition.post;
+    //   }
+    // } else {
+    //   position = transition.pre;
+    // }
+
+    TweenLite.to(this.node.current, duration / 1000, start);
+  }
+
+  /**
+   * 
+   */
+  getStartPosition = (isVisible, isNew) => {
+    // First, find out if we are animating in (isVisible = true)
+    const intention = isVisible ? 'out' : 'in';
+
+    // Then find out which history action was just fired (use helper)
+    const action = getCurrentAction();
+
+    return transition[intention][action.toLowerCase()];
+  }
+
+  /**
+   * 
+   */
+  getEndPosition = () => {
+    // Determine the end position of the element
+    return transition.in.push;
   }
 
   /**
@@ -97,8 +158,7 @@ class Route extends Component {
    * 
    */
   routeDidEnter = () => {
-    console.warn('RouteDidEnter');
-    return;
+
   }
 
   /**
@@ -106,20 +166,17 @@ class Route extends Component {
    * @returns {JSX}
    */
   render() {
-    const { transitionType } = this;
-    const {
-      component: Content,
-      isVisible,
-    } = this.props;
-
+    const { component: Content } = this.props;
     // return (
     //   <div ref={this.node}>
     //     <Content />
     //   </div>
     // );
+
+    const { transitionType } = this;
     return (
       <Transition
-        in={isVisible}
+        in={this.props.isVisible}
         timeout={transitionType.duration}
         onEntered={this.routeDidEnter}
       >
