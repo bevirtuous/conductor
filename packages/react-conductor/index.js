336130ea6259834b1357ca5c627925aa5147cc33
Richard Gorman <richardgorman@outlook.com>
2018-08-06 20:49:10 +0200

    Added named exports for helpers and react components

---
+++ packages/react-conductor/index.js
@@ -0,0 +1,4 @@
+export { default as Component } from './src/Component';
+export { default as Link } from './src/Link';
+export { default as Route } from './src/Route';
+export { default as Router } from './src/Router';
