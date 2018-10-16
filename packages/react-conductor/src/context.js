2c084584f044dab2408d540491a12c7347bbfb35
Richard Gorman <richardgorman@outlook.com>
2018-10-16 22:34:52 +0200

    Deleted Link component and added index for react-conductor

---
+++ packages/react-conductor/src/context.js
@@ -0,0 +1,4 @@
+import { createContext } from 'react';
+
+export const RouteContext = createContext();
+export const RouterContext = createContext();
---
+++ packages/react-conductor/src/index.js
@@ -0,0 +1,3 @@
+export { default as Route } from './Route';
+export { default as Router } from './Router';
+export * from './context';
