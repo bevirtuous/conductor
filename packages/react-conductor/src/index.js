ab5765a2fef869f5f1112483af47e7df1cf49e3f
Richard Gorman <richardgorman@outlook.com>
2018-12-04 10:11:53 +0100

    Removed CachedRoute

--- packages/react-conductor/src/index.js
+++ packages/react-conductor/src/index.js
@@ -1,5 +1,4 @@
 export { default as Route } from './Route';
 export { default as Router } from './Router';
-export { default as CachedRoute } from './CachedRoute';
 export { default as SpringRoute } from './SpringRoute';
 export * from './context';
