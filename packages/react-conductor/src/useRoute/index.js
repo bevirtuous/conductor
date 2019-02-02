99c42bfe7ca740b2208def037b7157774605f676
Richard Gorman <richardgorman@outlook.com>
2019-02-02 20:29:04 +0100

    Added useRoute hook and initial tests

--- packages/react-conductor/src/useRoute/index.js
+++ packages/react-conductor/src/useRoute/index.js
@@ -1,9 +1,15 @@
 import { useContext } from 'react';
+import { router } from '@virtuous/conductor';
 import { RouterContext } from '../context';
 
-function useRoute() {
-  // This causes the error.
-  return useContext(RouterContext);
+function useRoute(id = null) {
+  const routes = useContext(RouterContext);
+
+  if (!id) {
+    return routes[router.routeIndex][1];
+  }
+
+  return routes.find(([routeId]) => (id === routeId))[1] || null;
 }
 
 export default useRoute;
--- packages/react-conductor/src/useRoute/spec.jsx
+++ packages/react-conductor/src/useRoute/spec.jsx
@@ -1,21 +1,42 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render } from 'react-testing-library';
+import { router, stack } from '@virtuous/conductor';
+import { Router } from '../index';
 import useRoute from './index';
 
+let useRoute1 = null;
+let useRoute2 = null;
+
 /**
- * 
+ * @returns {null}
  */
-function MyComponent() {
-  const route = useRoute();
-  return JSON.stringify(route);
+function MyComponent({ id = null }) {
+  useRoute1 = useRoute();
+  useRoute2 = useRoute(id);
+  return null;
 }
 
 describe('useRoute()', () => {
-  it('should return the current route', () => {
-    const component = shallow((
-      <MyComponent />
+  it('should use the current route or the given route id', async () => {
+    router.register('/test');
+
+    const { id } = stack.getByIndex(router.routeIndex);
+
+    render((
+      <Router>
+        <MyComponent id={id} />
+      </Router>
     ));
 
-    expect(component).toMatchSnapshot();
+    expect(useRoute1.id).toEqual(stack.getByIndex(router.routeIndex).id);
+    expect(useRoute2.id).toEqual(stack.getByIndex(router.routeIndex).id);
+
+    await router.push({ pathname: '/test' });
+
+    expect(useRoute1.id).toEqual(stack.getByIndex(router.routeIndex).id);
+    // Ensure that the used context id was updated when a new route was pushed.
+    expect(useRoute2.id).toEqual(stack.get(id).id);
   });
+
+  // should return null when the given route id is invalid
 });
