98a814b74299e711a52dcc3f21c09a5029c041b1
Richard Gorman <richardgorman@outlook.com>
2019-02-24 22:15:22 +0100

    Added Routing actions behind useRouter hook

---
+++ packages/react-conductor/src/useRouter/spec.js
@@ -0,0 +1,35 @@
+import { router } from '@virtuous/conductor';
+import {
+  push,
+  pop,
+  replace,
+  reset,
+  resetTo,
+} from './index';
+
+const params = {
+  pathname: '123',
+  state: { something: true },
+};
+
+describe('useRouter()', () => {
+  it('should provide router actions', () => {
+    const spyPush = jest.spyOn(router, 'push');
+    const spyPop = jest.spyOn(router, 'pop');
+    const spyReplace = jest.spyOn(router, 'replace');
+    const spyReset = jest.spyOn(router, 'reset');
+    const spyResetTo = jest.spyOn(router, 'resetTo');
+
+    push(params);
+    pop(params);
+    replace(params);
+    reset();
+    resetTo(params);
+
+    expect(spyPush).toHaveBeenCalledWith(params);
+    expect(spyPop).toHaveBeenCalledWith(params);
+    expect(spyReplace).toHaveBeenCalledWith(params);
+    expect(spyReset).toHaveBeenCalled();
+    expect(spyResetTo).toHaveBeenCalledWith(params.pathname, params.state);
+  });
+});
