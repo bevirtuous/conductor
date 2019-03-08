9ece44ab56dbf6c9a313f51aa8d6e12f18e6f9f8
Richard Gorman <richardgorman@outlook.com>
2019-03-08 18:36:12 +0100

    Added useRouter as named export

--- packages/react-conductor/src/index.js
+++ packages/react-conductor/src/index.js
@@ -2,5 +2,6 @@ export { default as Route } from './Route';
 export { default as Router } from './Router';
 export { default as SpringRoute } from './SpringRoute';
 export { default as useRoute } from './useRoute';
+export { default as useRouter } from './useRouter';
 export * from './context';
 export * from './events';
