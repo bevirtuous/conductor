83cd9855fd5c6312b62317d660ee434f49bd9481
Richard Gorman <richardgorman@outlook.com>
2019-05-05 14:03:54 +0200

    Corrected wrong test cases for Route.NotFound

--- packages/react-conductor/src/404/spec.jsx
+++ packages/react-conductor/src/404/spec.jsx
@@ -1,31 +1,41 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { router } from '@virtuous/conductor';
 import Router from '../Router';
 import Route from '../Route';
-import RouteNotFound from '.';
+
+function ErrorPage() {
+  return (
+    <div>404 page</div>
+  );
+}
 
 describe('<Route.NotFound />', () => {
-  it('should render the given component', () => {
+  beforeEach(() => {
+    router.constructor();
+  });
+
+  it('should render nothing when route matches', () => {
     const app = shallow((
       <Router>
         <Route
           pattern="/myroute/123"
-          component={() => <div />}
+          component={() => <div>My Route page</div>}
         />
-        <RouteNotFound component={() => null} />
+        <Route.NotFound component={ErrorPage} />
       </Router>
     ));
 
-    expect(app.html()).toBeTruthy();
+    expect(app.html()).toBe('<div>My Route page</div>');
   });
 
-  it('should render null', () => {
+  it('should render the given component when no route matches', () => {
     const app = shallow((
       <Router>
-        <RouteNotFound component={() => null} />
+        <Route.NotFound component={ErrorPage} />
       </Router>
     ));
 
-    expect(app.html()).toBeFalsy();
+    expect(app.html()).toBe('<div>404 page</div>');
   });
 });
